67e50ea71b88263713392a5ef540ea792e012e8e
m-hz <connectmetotheworld@gmail.com>
2017-04-25 22:21:47 -0400

    bootstrap 4 integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor (@Inject(DOCUMENT) private document) { }
+  ngOnInit() {
+    let brands = ['light', 'dark', 'green'];
+    let x:number = Math.floor(Math.random() * 3);
+    this.document.getElementById('brandcss').setAttribute('href', 'assets/brands/' + brands[x] + '/brand.css');
+  }
+  title = 'Branded App Works!';
 }
